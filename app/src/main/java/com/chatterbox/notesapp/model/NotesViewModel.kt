686581ef124d50b63f9c099f53b1413a121d563b
Michael Duruji <mcduruji@hotmail.com>
2025-11-24 23:29:30 -0500

    creates git model for database control

--- app/src/main/java/com/chatterbox/notesapp/model/NotesViewModel.kt
+++ app/src/main/java/com/chatterbox/notesapp/model/NotesViewModel.kt
@@ -1,4 +1,55 @@
 package com.chatterbox.notesapp.model
 
-class TodoViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chatterbox.notesapp.data.local.dao.NoteDao
+import com.chatterbox.notesapp.data.local.entity.NoteEntity
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class NotesViewModel(
+    private val noteDao: NoteDao
+) : ViewModel() {
+
+    val notes = noteDao
+        .getAllNotes()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+
+    fun addNote(title: String, content: String) {
+        viewModelScope.launch {
+            noteDao.insertNote(
+                NoteEntity(
+                    title = title,
+                    content = content
+                )
+            )
+        }
+    }
+
+    fun updateNote(note: NoteEntity) {
+        viewModelScope.launch {
+            noteDao.updateNote(note)
+        }
+    }
+
+    private val _selectedNote = MutableStateFlow<NoteEntity?>(null)
+    val selectedNote: StateFlow<NoteEntity?> = _selectedNote.asStateFlow()
+
+    fun onNoteSelected(note: NoteEntity) {
+        _selectedNote.value = note
+    }
+
+    fun deleteNote(note: NoteEntity) {
+        viewModelScope.launch {
+            noteDao.deleteNote(note)
+        }
+    }
 }
